8f25f9907722a3d38b8a1b92de5c5aea2661fe5d
Danny Yoo <dyoo@cs.wpi.edu>
2012-02-23 12:24:05 -0500

    reindenting everything

--- basic-blocks.rkt
+++ basic-blocks.rkt
@@ -20,7 +20,7 @@
                        )
   #:transparent)
 
-  
+
 
 (define-struct next ())
 (define-struct dynamic ())
@@ -39,95 +39,95 @@
                   #:jump? (jump? default-jump?)
                   #:jump-targets (jump-targets default-jump-targets))
   (check-good-stmts! stmts label?)
-
+  
   
   ;; Main loop.  Watch for leaders.
   (let-values ([(entry-names-set) (list->set (cons (label-name (first stmts)) entry-names))]
                [(leaders stmts)
                 (find/inject-leaders stmts entry-names jump? jump-targets
                                      label? label-name fresh-label)])
-
+    
     
     ;; leader?: stmt -> boolean
     ;; Returns true if the statement is a leader.
     (define (leader? stmt)
       (and (label? stmt) (set-member? leaders (label-name stmt))))
-
+    
     ;; Skip statements until we hit the next leader.
     (define (skip-till-leader stmts)
       (cond
-       [(empty? stmts) '()]
-       [(leader? (first stmts))
-        stmts]
-       [else
-        (skip-till-leader (rest stmts))]))
-
+        [(empty? stmts) '()]
+        [(leader? (first stmts))
+         stmts]
+        [else
+         (skip-till-leader (rest stmts))]))
+    
     (filter-reachable
-    (let loop ([bblocks '()]
-               [pending-block-name (label-name (first stmts))]
-               [pending-stmts/rev (list)]
-               [pending-jump-targets (set)]
-               [pending-next-succ #f]
-               [stmts (rest stmts)])
-      (cond
-       [(empty? stmts)
-        (reverse (cons (make-bblock pending-block-name
-                                    (set-member? entry-names-set pending-block-name)
-                                    (reverse pending-stmts/rev)
-                                    pending-jump-targets
-                                    pending-next-succ)
-                       bblocks))]
-       [(leader? (first stmts))
-        (loop (cons (make-bblock pending-block-name
-                                 (set-member? entry-names-set pending-block-name)
-                                 (reverse pending-stmts/rev)
-                                 pending-jump-targets
-                                 pending-next-succ)
-                    bblocks)
-              (label-name (first stmts))
-              (list)
-              (set)
-              #f
-              (rest stmts))]
-       
-       [else
-        (loop bblocks 
-              pending-block-name 
-              ;; Omit dead labels.
-              (if (label? (first stmts))
-                  pending-stmts/rev 
-                  (cons (first stmts) pending-stmts/rev))
-              (cond [(jump? (first stmts))
-                     (set-union (list->set (map (lambda (t)
-                                                  (cond [(eq? t NEXT)
-                                                         (label-name (second stmts))]
-                                                        [(eq? t DYNAMIC)
-                                                         DYNAMIC]
-                                                        [else t]))
-                                                (jump-targets (first stmts))))
-                                pending-jump-targets)]
-                    [(and (not (null? (rest stmts)))
-                          (leader? (second stmts)))
-                     (set-union pending-jump-targets (set (label-name (second stmts))))]
-                    [else
-                     pending-jump-targets])
-              (cond [(and (jump? (first stmts))
-                          (memq NEXT (jump-targets (first stmts))))
-                     (label-name (second stmts))]
-                    [(and (not (jump? (first stmts)))
-                          (not (empty? (rest stmts)))
-                          (leader? (second stmts)))
-                     (label-name (second stmts))]
-                    [else #f])
-              
-              (cond [(and (jump? (first stmts))
-                          (not (memq NEXT (jump-targets (first stmts)))))
-                     ;; After a jump, skip till we hit a leader
-                     (skip-till-leader (rest stmts))]
-                    [else
-                     (rest stmts)]))])))))
+     (let loop ([bblocks '()]
+                [pending-block-name (label-name (first stmts))]
+                [pending-stmts/rev (list)]
+                [pending-jump-targets (set)]
+                [pending-next-succ #f]
+                [stmts (rest stmts)])
+       (cond
+         [(empty? stmts)
+          (reverse (cons (make-bblock pending-block-name
+                                      (set-member? entry-names-set pending-block-name)
+                                      (reverse pending-stmts/rev)
+                                      pending-jump-targets
+                                      pending-next-succ)
+                         bblocks))]
+         [(leader? (first stmts))
+          (loop (cons (make-bblock pending-block-name
+                                   (set-member? entry-names-set pending-block-name)
+                                   (reverse pending-stmts/rev)
+                                   pending-jump-targets
+                                   pending-next-succ)
+                      bblocks)
+                (label-name (first stmts))
+                (list)
+                (set)
+                #f
+                (rest stmts))]
+         
+         [else
+          (loop bblocks 
+                pending-block-name 
+                ;; Omit dead labels.
+                (if (label? (first stmts))
+                    pending-stmts/rev 
+                    (cons (first stmts) pending-stmts/rev))
+                (cond [(jump? (first stmts))
+                       (set-union (list->set (map (lambda (t)
+                                                    (cond [(eq? t NEXT)
+                                                           (label-name (second stmts))]
+                                                          [(eq? t DYNAMIC)
+                                                           DYNAMIC]
+                                                          [else t]))
+                                                  (jump-targets (first stmts))))
+                                  pending-jump-targets)]
+                      [(and (not (null? (rest stmts)))
+                            (leader? (second stmts)))
+                       (set-union pending-jump-targets (set (label-name (second stmts))))]
+                      [else
+                       pending-jump-targets])
+                (cond [(and (jump? (first stmts))
+                            (memq NEXT (jump-targets (first stmts))))
+                       (label-name (second stmts))]
+                      [(and (not (jump? (first stmts)))
+                            (not (empty? (rest stmts)))
+                            (leader? (second stmts)))
+                       (label-name (second stmts))]
+                      [else #f])
+                
+                (cond [(and (jump? (first stmts))
+                            (not (memq NEXT (jump-targets (first stmts)))))
+                       ;; After a jump, skip till we hit a leader
+                       (skip-till-leader (rest stmts))]
+                      [else
+                       (rest stmts)]))])))))
 
-  
+
 ;; Make sure we get a good list of statements for fracture.
 (define (check-good-stmts! stmts label?)
   (match stmts
@@ -188,29 +188,29 @@
   (define visited (make-hasheq))  
   (define (dfs queue)
     (cond
-     [(empty? queue)
-      (void)]
-     [(hash-has-key? visited (first queue))
-      (dfs (rest queue))]
-     [else
-      (hash-set! visited (first queue) #t)
-      (dfs (append (for/list ([neighbor (bblock-succs (hash-ref ht (first queue)))]
-                              #:when (and (not (eq? neighbor DYNAMIC))
-                                          (not (hash-has-key? visited neighbor))))
-                             neighbor)
-                   (rest queue)))]))
+      [(empty? queue)
+       (void)]
+      [(hash-has-key? visited (first queue))
+       (dfs (rest queue))]
+      [else
+       (hash-set! visited (first queue) #t)
+       (dfs (append (for/list ([neighbor (bblock-succs (hash-ref ht (first queue)))]
+                               #:when (and (not (eq? neighbor DYNAMIC))
+                                           (not (hash-has-key? visited neighbor))))
+                      neighbor)
+                    (rest queue)))]))
   (dfs (for/list ([b bblocks]
                   #:when (bblock-entry? b))
-                 (bblock-name b)))
-
-
+         (bblock-name b)))
+  
+  
   (for/list ([b bblocks]
              #:when (hash-has-key? visited (bblock-name b)))
-            b))
+    b))
 
 
 
- 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (default-label? x)
